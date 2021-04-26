a634ca3a93d6186c1e4b5c29d2a16d5a545e38d5
Jay <jmandav1@jhu.edu>
2021-04-26 01:28:20 -0400

    created random forest model, need to do predict for a single user and UI work for it

---
+++ createModels.R
@@ -0,0 +1,36 @@
+set.seed(4)
+
+library(randomForest)
+
+address <- "https://raw.githubusercontent.com/vrose99/AdvancedDataScienceFinalProject/8acd472133dda8bf77b87648d281237419c7fced/Data/chronic_kidney_disease.csv"
+raw_data <- read.csv(address)
+filtered_data <- raw_data[rowSums(is.na(raw_data[ , 1:25])) == 0, ]
+
+
+test_inds <- sample(1:nrow(filtered_data), floor(0.2 * nrow(filtered_data)))
+train <- filtered_data[-test_inds, ]
+test <- filtered_data[test_inds, ]
+
+# rf_model <- randomForest(class ~ ., data=train, ntree=100, mtry=2, importance=TRUE)
+rf_model <- randomForest(train[ ,-25], as.factor(train[ ,25]), test[ ,-25], as.factor(test[ ,25]))
+rf_model
+
+# rf_model$test$predicted
+# test$class
+
+err <- 0
+for (i in 1:length(test$class)) {
+  if (test$class[i] != rf_model$test$predicted[i]) {
+    err <- err + 1
+  }
+}
+print(1 - (err/length(test$class)))
+
+
+save(rf_model, file="randomForestModel.Rdata")
+
+
+
+
+
+
